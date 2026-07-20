f0b59cb5e2e509c6710f07bd5f7f7c26236bf50a
jmpargana <jmpargana@gmail.com>
2026-07-20 13:23:20 +0100

    refactor: crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
-pub mod broker;
-pub mod network;
-pub mod partition;
-pub mod protocol;
-pub mod replica;
-pub mod segment;
-pub mod storage;
-pub mod topic;
+extern crate storage as _storage;
+extern crate broker as _broker;
+extern crate network as _network;
+
+pub use _storage::partition;
+pub use _storage::protocol;
+pub use _storage::replica;
+pub use _storage::segment;
+pub use _storage::storage;
+pub use _storage::topic;
+
+pub use _broker::broker;
+pub use _network::network;
